a33949a0c121f361580d3cfc07902747ecb5da44
gadfly <szcshui@gmail.com>
2011-12-01 09:03:16 -0800

    add feature of Tree

--- src/edu/luc/cs/laufer/cs473/shapes/trees.scala
+++ src/edu/luc/cs/laufer/cs473/shapes/trees.scala
@@ -1,34 +1,48 @@
 package edu.luc.cs.laufer.cs473.shapes
 
-class Tree {
+abstract class Tree 
 
+abstract class Fruit(weight: Float) extends Tree
+
+case class Apple(weight: Float) extends Fruit(weight)
+
+case class Peach(weight: Float) extends Fruit(weight)
+
+case class Mango(weight: Float) extends Fruit(weight)
+
+case class Branch(children: Seq[Tree]) extends Tree {
+  require(children != null)
+  require(!children.contains(null))
 }
 
-case class Apple(weight: Float) extends Tree
-
-case class Peach(weight: Float) extends Tree
-
-case class Mango(weight: Float) extends Tree
-
-case class Branch(ts: Seq[Tree]) extends Tree {
-  require(ts != null)
-  require(!ts.contains(null))
-}
-
-case class Cluster(multiplier: Int, t: Tree) extends Tree {
-  require(t != null)
+case class Cluster(multiplier: Int, child: Tree) extends Tree {
+  require(child != null)
 }
 
 trait TreeAlgebra[T] {
   def visitMango(m: Mango): T
   def visitPeach(p: Peach): T
-  def visitBrach(rs: Seq[T], b: Branch): T
+  def visitBranch(rs: Seq[T], b: Branch): T
   def visitCluster(r: T, c: Cluster): T
+  
+  def fold: Tree => T = {
+    case m: Mango => visitMango(m)
+    case p: Peach => visitPeach(p)
+    case b: Branch => visitBranch(b.children.map(fold(_)), b)
+    case c: Cluster => visitCluster(fold(c.child), c)
+  }
+}
+
+object Weight extends TreeAlgebra[Float] {
+  override def visitMango(m : Mango) = m.weight
+  override def visitPeach(p: Peach) = p.weight
+  override def visitBranch(rs: Seq[Float], b :Branch) = rs.reduceLeft(_ + _)
+  override def visitCluster(r: Float, c: Cluster) = c.multiplier * r
 }
 
 class Grow(factor: Int) extends TreeAlgebra[Tree] {
   override def visitMango(m: Mango) = Mango(m.weight * factor)
   override def visitPeach(p: Peach) = Peach(p.weight * factor)
-  override def visitBrach(rs: Seq[Tree], b: Branch) = Branch(rs)
+  override def visitBranch(rs: Seq[Tree], b: Branch) = Branch(rs)
   override def visitCluster(r: Tree, c: Cluster) = Cluster(c.multiplier, r)
 }
